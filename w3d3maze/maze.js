ff8fbdb0880a9116041c2679658efc84e9e1f359
samson <samsonahmed@miu.edu>
2021-10-14 06:38:05 -0500

    modified index

--- w3d3maze/maze.js
+++ w3d3maze/maze.js
@@ -1,25 +1,19 @@
 $(function() {
     let start=false;
     let lost=false;
-    let count=0;
     $("#start").click(function(){
       start=true;
       lost=false;
       $(".boundary").removeClass("youlose");
       $(".boundary").mouseenter(function(){
-          count=1;
             lost=true;
             $(".boundary").addClass("youlose");
-            console.log(lost);
-            // preventDefault();
       })
       $("#maze").mouseleave(function(){
           $(".boundary").addClass("youlose");      
       })
       $("#end").mouseover(function(){
-          console.log(start +" "+ lost);
           if(start && !lost){
-          location.reload(true);
           alert("you won the game!");
           preventDefault();
           location.reload(true);
